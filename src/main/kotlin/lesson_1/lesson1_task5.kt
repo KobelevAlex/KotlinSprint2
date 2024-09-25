200406dcbb6d5506aa6d08ea250333121815daa1
Alexander Kobelev <kobelef@mail.ru>
2024-09-25 23:08:15 +0400

    KS-1-5 Убрал магические числа в константы, сделал всё логично!

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,17 @@
 package lesson_1
 
+const val secondsInTheClock: Short = 3600
+const val secondsInOneHour: Byte = 60
 fun main() {
+
     val seconds: Short = 6480
     val timeString: String = сonvertingSecondsToTimeString(seconds)
     println(timeString)
 }
 
 fun сonvertingSecondsToTimeString(meaning: Short): String {
-    val hours = meaning / 3600
-    val minutes = (meaning % 3600) / 60
-    val secondsRemaining = meaning % 60
-
+    val hours = meaning / secondsInTheClock
+    val minutes = (meaning % secondsInTheClock) / secondsInOneHour
+    val secondsRemaining = meaning % secondsInOneHour
     return String.format("%02d:%02d:%02d", hours, minutes, secondsRemaining)
 }
