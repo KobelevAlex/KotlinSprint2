3a07984793e058fac36c901a4b243615aeda46da
Alexander Kobelev <kobelef@mail.ru>
2024-09-27 22:46:41 +0400

    KS-1-5 изменил формат констант и убрал магичское число - 3600

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,21 @@
 package lesson_1
 
-const val secondsInTheClock: Short = 3600
-const val secondsInOneHour: Byte = 60
+const val SECONDS_IN_ONE_MINUTE: Byte = 60
+const val MINUTES_ONE_HOUR: Byte = 60
+
 fun main() {
 
     val seconds: Short = 6480
     val timeString: String = сonvertingSecondsToTimeString(seconds)
+
     println(timeString)
 }
 
 fun сonvertingSecondsToTimeString(meaning: Short): String {
-    val hours = meaning / secondsInTheClock
-    val minutes = (meaning % secondsInTheClock) / secondsInOneHour
-    val secondsRemaining = meaning % secondsInOneHour
+    val secondsInOneHour: Int = SECONDS_IN_ONE_MINUTE * SECONDS_IN_ONE_MINUTE
+    val hours = meaning / secondsInOneHour
+    val minutes = (meaning % secondsInOneHour) / MINUTES_ONE_HOUR
+    val secondsRemaining = meaning % SECONDS_IN_ONE_MINUTE
+
     return String.format("%02d:%02d:%02d", hours, minutes, secondsRemaining)
 }
