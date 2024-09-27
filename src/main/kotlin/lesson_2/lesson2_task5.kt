a27d63fced70a8a7345d228577194dbcaf8e199a
Alexander Kobelev <kobelef@mail.ru>
2024-09-27 22:57:12 +0400

    KS-2-5 исправил проценты и формулу

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,11 +1,11 @@
 package lesson_2
 
 fun main() {
-    val amount: Double = 70_000.0
-    val annualInterestRate: Double = 0.167
+    val amount: Double = 70000.0
+    val annualInterestRate: Double = 16.7
     val years: Int = 20
 
-    val amountOfTheDeposit: Double = amount * Math.pow(1 + annualInterestRate, years.toDouble())
+    val amountOfTheDeposit: Double = amount * Math.pow(1 + annualInterestRate/100, years.toDouble())
 
     println("Размер вклада через $years лет - ${String.format("%.3f", amountOfTheDeposit)}")
 }
