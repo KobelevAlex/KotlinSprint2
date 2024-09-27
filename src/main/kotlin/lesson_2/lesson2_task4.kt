ea0bd1b2cf7da06ee8dedb5574fd13d272a16a5b
Alexander Kobelev <kobelef@mail.ru>
2024-09-27 23:06:10 +0400

    KS-2-4 исправил проценты и подправил вывод

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,12 +3,13 @@ package lesson_2
 fun main() {
     val crystalOre: Int = 7
     val ironOre: Int = 11
-    val buff: Double = 0.2
+    val buff: Double = 20.0
+    val numberOfPercent: Int = 100
 
     println("""
         У вас сейчас в запасах:
-         - кристаллической руды $crystalOre ед., ещё бонусом + 20% - ${(crystalOre * buff).toInt()} ед.
-         - железной руды $ironOre ед, ещё бонусом + 20% - ${(ironOre * buff).toInt()} ед.
+         - кристаллической руды $crystalOre ед., ещё бонусом + $buff% - ${(crystalOre * buff/numberOfPercent).toInt()} ед.
+         - железной руды $ironOre ед, ещё бонусом + $buff% - ${(ironOre * buff/numberOfPercent).toInt()} ед.
                  
     """.trimIndent())
 }
