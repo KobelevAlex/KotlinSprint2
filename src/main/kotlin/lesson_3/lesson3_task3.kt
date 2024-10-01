df524ba427bb82aea8caf590f763054b9f15b32b
Alexander Kobelev <kobelef@mail.ru>
2024-10-01 20:04:28 +0400

    KS-3-2 table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,26 @@
+package lesson_3
+
+fun main() {
+    var meaning = 7
+
+    fun printOutput() {
+        println(
+            """
+        $meaning x 1 = ${meaning * 1}
+        $meaning x 2 = ${meaning * 2}
+        $meaning x 3 = ${meaning * 3}
+        $meaning x 4 = ${meaning * 4}
+        $meaning x 5 = ${meaning * 5}
+        $meaning x 6 = ${meaning * 6}
+        $meaning x 7 = ${meaning * 7}
+        $meaning x 8 = ${meaning * 8}
+        $meaning x 9 = ${meaning * 9}
+        
+    """.trimIndent()
+        )
+
+    }
+    printOutput()
+    meaning = 8
+    printOutput()
+}
