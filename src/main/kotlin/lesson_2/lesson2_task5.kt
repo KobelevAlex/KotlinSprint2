fe2305ed285baf3cd08529a55de1457ff83d088c
Alexander Kobelev <kobelef@mail.ru>
2024-10-01 18:44:23 +0400

    KS-2-5 расставил пробелы

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,7 +5,7 @@ fun main() {
     val annualInterestRate: Double = 16.7
     val years: Int = 20
 
-    val amountOfTheDeposit: Double = amount * Math.pow(1 + annualInterestRate/100, years.toDouble())
+    val amountOfTheDeposit: Double = amount * Math.pow(1 + annualInterestRate / 100, years.toDouble())
 
     println("Размер вклада через $years лет - ${String.format("%.3f", amountOfTheDeposit)}")
 }
