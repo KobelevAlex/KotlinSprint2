e674df5df50363ffe9fe5cc8a4d192afb0c8bf7c
Alexander Kobelev <kobelef@mail.ru>
2024-10-02 21:08:59 +0400

    KS-3-2 surname исправил инициализацию и вывод на печать

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -3,16 +3,13 @@ package lesson_3
 fun main() {
     val name = "Татьяна"
     val patronymic = "Сергеевна"
-    val surname = "Андреева"
-    val age = 20
-    val newSurname = "Сидорова"
-    val newAge = 22
+    var surname = "Андреева"
+    var age = 20
 
-    println(
-        """
-        $surname $name $patronymic, $age лет.
-        $newSurname $name $patronymic. $newAge лет.
-    """.trimIndent()
-    )
+    println("$surname $name $patronymic, $age лет.")
 
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $patronymic, $age лет.")
 }
