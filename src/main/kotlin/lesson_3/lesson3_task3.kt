368908bb22d9312c81e8d26da5d73444b9ead722
Alexander Kobelev <kobelef@mail.ru>
2024-10-02 22:01:06 +0400

    KS-3-3-table таблица уможения

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -3,9 +3,8 @@ package lesson_3
 fun main() {
     var meaning = 7
 
-    fun printOutput() {
-        println(
-            """
+    println(
+        """
         $meaning x 1 = ${meaning * 1}
         $meaning x 2 = ${meaning * 2}
         $meaning x 3 = ${meaning * 3}
@@ -15,12 +14,22 @@ fun main() {
         $meaning x 7 = ${meaning * 7}
         $meaning x 8 = ${meaning * 8}
         $meaning x 9 = ${meaning * 9}
-        
-    """.trimIndent()
-        )
+        """.trimIndent()
+    )
 
-    }
-    printOutput()
     meaning = 8
-    printOutput()
+    println()
+    println(
+        """
+        $meaning x 1 = ${meaning * 1}
+        $meaning x 2 = ${meaning * 2}
+        $meaning x 3 = ${meaning * 3}
+        $meaning x 4 = ${meaning * 4}
+        $meaning x 5 = ${meaning * 5}
+        $meaning x 6 = ${meaning * 6}
+        $meaning x 7 = ${meaning * 7}
+        $meaning x 8 = ${meaning * 8}
+        $meaning x 9 = ${meaning * 9}
+        """.trimIndent()
+    )
 }
