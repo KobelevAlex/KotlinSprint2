a6c81430644fc89721364a77cfc79abb5348ad20
Alexander Kobelev <kobelef@mail.ru>
2024-10-05 19:35:20 +0400

    KS-4-4-workout выделил в переменную проверку на четность

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,40 +3,43 @@ package lesson_4
 fun main() {
 
     var day = 1
+    var parityCheck = day % 2
 
     println(
         """
             День - $day
-        Упражнения для рук: ${day % 2 != 0}
-        Упражнения для ног: ${day % 2 == 0}
-        Упражнения для спины: ${day % 2 == 0}
-        Упражнения для пресса: ${day % 2 != 0}
+        Упражнения для рук: ${parityCheck != 0}
+        Упражнения для ног: ${parityCheck == 0}
+        Упражнения для спины: ${parityCheck == 0}
+        Упражнения для пресса: ${parityCheck != 0}
     """.trimIndent()
     )
     println()
 
     day = 5
+    parityCheck = day % 2
 
     println(
         """
             День - $day
-        Упражнения для рук: ${day % 2 != 0}
-        Упражнения для ног: ${day % 2 == 0}
-        Упражнения для спины: ${day % 2 == 0}
-        Упражнения для пресса: ${day % 2 != 0}
+        Упражнения для рук: ${parityCheck != 0}
+        Упражнения для ног: ${parityCheck == 0}
+        Упражнения для спины: ${parityCheck == 0}
+        Упражнения для пресса: ${parityCheck != 0}
     """.trimIndent()
     )
     println()
 
     day = 6
+    parityCheck = day % 2
 
     println(
         """
             День - $day
-        Упражнения для рук: ${day % 2 != 0}
-        Упражнения для ног: ${day % 2 == 0}
-        Упражнения для спины: ${day % 2 == 0}
-        Упражнения для пресса: ${day % 2 != 0}
+        Упражнения для рук: ${parityCheck != 0}
+        Упражнения для ног: ${parityCheck == 0}
+        Упражнения для спины: ${parityCheck == 0}
+        Упражнения для пресса: ${parityCheck != 0}
     """.trimIndent()
     )
 
