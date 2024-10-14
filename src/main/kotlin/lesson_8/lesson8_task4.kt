f1f8e229de96302e42024664da0916bef40daedc
Alexander Kobelev <kobelef@mail.ru>
2024-10-15 00:10:10 +0400

    KS-8-4-replacement исправил на arrayOf и использовал indexOf()

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -2,11 +2,11 @@ package lesson_8
 
 fun main() {
 
-    val listOfIngredientsАorСharlotte = arrayListOf("яйцо", "мука", "сода", "сахар", "молоко", "яблоки")
+    val listOfIngredientsForСharlotte = arrayOf("яйцо", "мука", "сода", "сахар", "молоко", "яблоки")
     fun listOfIngredients() {
         println("Список ингредиентов в шарлотке:")
-        for (i in listOfIngredientsАorСharlotte.indices) {
-            println("${i + 1} - ${listOfIngredientsАorСharlotte[i]}")
+        for (i in listOfIngredientsForСharlotte.indices) {
+            println("${i + 1} - ${listOfIngredientsForСharlotte[i]}")
         }
     }
 
@@ -15,21 +15,15 @@ fun main() {
     println("Напишите название ингредиента,который хотите заменить:")
 
     var userInterIngredient = readln()
-    var ingredientTrueOrFalse = listOfIngredientsАorСharlotte.contains(userInterIngredient)
 
-    while (ingredientTrueOrFalse != true) {
+    while (userInterIngredient !in listOfIngredientsForСharlotte) {
         println(
             "Такого ингредиента в рецепте нет. Напишите название ингредиента," +
                     "который есть в рецепте и хотите его заменить:"
         )
-
         userInterIngredient = readln()
-
-        ingredientTrueOrFalse = listOfIngredientsАorСharlotte.contains(userInterIngredient)
-
-        if (ingredientTrueOrFalse == true) break
-
     }
+
     println(
         """
         Ингредиент "$userInterIngredient" в рецепте есть!
@@ -38,13 +32,11 @@ fun main() {
     )
 
     val anIndigenousReplacement = readln()
+    val userIngredient = listOfIngredientsForСharlotte.indexOf(userInterIngredient)
 
-    for (i in listOfIngredientsАorСharlotte.indices) {
-        if (listOfIngredientsАorСharlotte[i] == userInterIngredient) {
-            listOfIngredientsАorСharlotte[i] = anIndigenousReplacement
-            break
-        }
-    }
+    listOfIngredientsForСharlotte[userIngredient] = anIndigenousReplacement
+
+    println("Готово! Вы сохранили следующий список.")
 
     listOfIngredients()
 
