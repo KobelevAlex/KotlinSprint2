6a1917c86456a06800c7975fb1dc83cf7cf29d74
Alexander Kobelev <kobelef@mail.ru>
2024-10-14 23:14:19 +0400

    KS-8-2-ingredients исправил

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -1,20 +1,24 @@
 package lesson_8
 
 fun main() {
-    val listOfIngredientsАorСharlotte = arrayListOf("яйцо", "мука", "сода", "сахар", "молоко", "яблоки")
+    val listOfIngredientsАorСharlotte = arrayOf("яйцо", "мука", "сода", "сахар", "молоко", "яблоки")
 
     println("Напишите название ингредиента:")
 
     val userInterIngredient = readln()
+    var found = false
 
-    for (i in listOfIngredientsАorСharlotte.indices) {
-        var ingredientTrueOralse: Boolean
-        if (listOfIngredientsАorСharlotte[i] == userInterIngredient) {
-            println("Ингредиент ${listOfIngredientsАorСharlotte[i]} в рецепте есть")
+    for (i in listOfIngredientsАorСharlotte) {
+
+        if (i == userInterIngredient) {
+            found = true
             break
-        } else if ((listOfIngredientsАorСharlotte.size - 1) == i) println("Такого ингредиента в рецепте нет")
-
+        }
     }
-
-
+    if (found == true) println("Ингредиент \"$userInterIngredient\" в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
 }
+
+
+
+
