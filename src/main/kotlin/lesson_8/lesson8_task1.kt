499d6fe7fff61ddb0cb2269eeeb9433d6522c856
Alexander Kobelev <kobelef@mail.ru>
2024-10-14 22:42:24 +0400

    KS-8-1-numberOfViews использовал intArrayOf

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -2,15 +2,7 @@ package lesson_8
 
 fun main() {
 
-    val numberOfViewsByDay = IntArray(7)
-
-    numberOfViewsByDay[0] = 20
-    numberOfViewsByDay[1] = 34
-    numberOfViewsByDay[2] = 41
-    numberOfViewsByDay[3] = 36
-    numberOfViewsByDay[4] = 55
-    numberOfViewsByDay[5] = 29
-    numberOfViewsByDay[6] = 19
+    val numberOfViewsByDay = intArrayOf(20, 34,41,36,55,29,19)
 
     val numberOfViewsPerWeek = numberOfViewsByDay.sum()
 
