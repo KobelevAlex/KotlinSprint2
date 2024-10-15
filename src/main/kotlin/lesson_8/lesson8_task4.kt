769c84dc4fc3b4c94e699ac857ed7289cd9de1fa
Alexander Kobelev <kobelef@mail.ru>
2024-10-15 19:28:53 +0400

    KS-8-4-replacement использовал только indexOf()

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -3,8 +3,9 @@ package lesson_8
 fun main() {
 
     val listOfIngredientsForСharlotte = arrayOf("яйцо", "мука", "сода", "сахар", "молоко", "яблоки")
+
     fun listOfIngredients() {
-        println("Список ингредиентов в шарлотке:")
+        println("Список ингредиентов для шарлотки:")
         for (i in listOfIngredientsForСharlotte.indices) {
             println("${i + 1} - ${listOfIngredientsForСharlotte[i]}")
         }
@@ -15,13 +16,15 @@ fun main() {
     println("Напишите название ингредиента,который хотите заменить:")
 
     var userInterIngredient = readln()
+    var userIngredient = listOfIngredientsForСharlotte.indexOf(userInterIngredient)
 
-    while (userInterIngredient !in listOfIngredientsForСharlotte) {
+    while (userIngredient < 0) {
         println(
             "Такого ингредиента в рецепте нет. Напишите название ингредиента," +
                     "который есть в рецепте и хотите его заменить:"
         )
         userInterIngredient = readln()
+        userIngredient = listOfIngredientsForСharlotte.indexOf(userInterIngredient)
     }
 
     println(
@@ -32,7 +35,7 @@ fun main() {
     )
 
     val anIndigenousReplacement = readln()
-    val userIngredient = listOfIngredientsForСharlotte.indexOf(userInterIngredient)
+
 
     listOfIngredientsForСharlotte[userIngredient] = anIndigenousReplacement
 
