8b32ac09ca9e30faaf3c5ef2ba9d30bc60ce8463
Alexander Kobelev <kobelef@mail.ru>
2024-10-24 21:31:49 +0400

    KS-11-1-User

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,29 @@
+package lesson_11
+
+import sun.net.www.protocol.mailto.MailToURLConnection
+
+class User(val id: Int, val userName: String, var userPassword: String, var userEMail: String) {
+
+    fun printUserInfo() {
+        println(
+            """
+            
+            ${userName}, у вас следующие данные:
+            ID - $id
+            Имя - $userName
+            Пароль - $userPassword
+            Почтовый адрес - $userEMail
+            
+        """.trimIndent()
+        )
+    }
+
+}
+
+fun main() {
+    val userFirst = User(123456, "Alexandr", "qwerty", "alex@mail.ru")
+    val userSecond = User(654321, "Maxim", "ytrewq", "maxim@mail.ru")
+    userFirst.printUserInfo()
+    userSecond.printUserInfo()
+
+}
