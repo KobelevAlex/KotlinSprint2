e5dc47109883fdf8e76509cb21dd0f435d87c6e9
Alexander Kobelev <kobelef@mail.ru>
2024-10-24 19:43:57 +0400

    KS-10-3-lengthPassword вынес функцию в космос

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -2,29 +2,39 @@ package lesson_10
 
 import kotlin.random.Random
 
+fun createPassword(
+    passwordLength: Int, minValue: Double, maxValue: Double, numberRange: CharRange,
+    charRangeSpecialCharacters: CharRange
+): String {
+    val listOfRandomCharacters = mutableListOf<Char>()
+
+    for (i in 1..passwordLength) {
+        val random = Math.random()
+        println(random)
+        if (random <= minValue) {
+            listOfRandomCharacters.add(charRangeSpecialCharacters.random())
+        } else if ((random > minValue) && (random <= maxValue)) {
+            listOfRandomCharacters.add(numberRange.random())
+        } else listOfRandomCharacters.add(' ')
+    }
+
+    return listOfRandomCharacters.joinToString("")
+}
+
 fun main() {
     val minValue = 0.466
     val maxValue = 0.895
     val charRangeSpecialCharacters = '!'..'/'
     val numberRange = '0'..'9'
-    var userPassword: String
-    var listOfRandomCharacters: MutableList<Char> = mutableListOf()
+    val userPassword: String
 
     println("Введите длину пароля:")
+
     val passwordLength = readln().toInt()
-    fun createPassword(value: Int) {
-        for (i in 1..value) {
-            val random = Math.random()
-            println(random)
-            if (random <= minValue) {
-                listOfRandomCharacters.add(charRangeSpecialCharacters.random())
-            } else if ((random > minValue) && (random <= maxValue)) {
-                listOfRandomCharacters.add(numberRange.random())
-            } else listOfRandomCharacters.add(' ')
-        }
-    }
-    createPassword(passwordLength)
-    userPassword = listOfRandomCharacters.joinToString("")
+
+    userPassword = createPassword(passwordLength, minValue, maxValue, numberRange, charRangeSpecialCharacters)
+
     println("Ваш пароль из $passwordLength символов- $userPassword")
 
 }
+
