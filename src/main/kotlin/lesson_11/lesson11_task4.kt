1dffe8c27da3cba28b8932f7c24fa53c8b95d739
Alexander Kobelev <kobelef@mail.ru>
2024-10-30 22:19:43 +0400

    KS-11-4-Recipe убрал лишнее, добавил необходимое

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -6,34 +6,15 @@ class RecipeCategory(
     val describeRecipeCategory: String,
     private val listRecipeCategory: MutableList<RecipeCategory> = mutableListOf(),
     var favourites: Boolean = false,
-) {
-    fun addRecipeCategory(recipeCategory: RecipeCategory) {
-        listRecipeCategory.add(recipeCategory)
-        println("Добавлена новая категория - ${recipeCategory.nameRecipeCategory}")
-    }
-
-    fun removeRecipeCategory(recipeCategory: RecipeCategory) {
-        listRecipeCategory.remove(recipeCategory)
-        println("Была удалена категория - ${recipeCategory.nameRecipeCategory}")
-    }
-}
+)
 
 class Recipe(
     val nameRecipe: String,
     var coverRecipe: String,
     private val listRecipe: MutableList<Recipe> = mutableListOf(),
+    private val cookingMethod: MutableList<String> = mutableListOf(),
     var favourites: Boolean = false,
-) {
-    fun addRecipe(recipe: Recipe) {
-        listRecipe.add(recipe)
-        println("Добавлена новая категория - ${recipe.nameRecipe}")
-    }
-
-    fun removeRecipe(recipe: Recipe) {
-        listRecipe.remove(recipe)
-        println("Была удалена категория - ${recipe.nameRecipe}")
-    }
-}
+)
 
 class Ingredient(
     val nameIngredient: String,
@@ -44,14 +25,4 @@ class Ingredient(
     var cookingMethod: String,
     private val listIngredient: MutableList<Ingredient> = mutableListOf(),
     var favourites: Boolean = false,
-) {
-    fun addIngredient(ingredient: Ingredient) {
-        listIngredient.add(ingredient)
-        println("Добавлена новая категория - ${ingredient.nameIngredient}")
-    }
-
-    fun removeIngredient(ingredient: Ingredient) {
-        listIngredient.remove(ingredient)
-        println("Была удалена категория - ${ingredient.nameIngredient}")
-    }
-}
+)
