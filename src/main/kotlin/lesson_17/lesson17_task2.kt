24bb4c808e22a136b407f06fc24900c851cdd61d
Alexander Kobelev <kobelef@mail.ru>
2024-12-07 20:16:52 +0400

    KS-17-2-Ship исправил

--- src/main/kotlin/lesson_17/lesson17_task2.kt
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -1,16 +1,10 @@
 package lesson_17
 
-class Ship(private var _name: String, private val _averageSpeed: Int, private val _homePackage: String) {
-    var name: String
-        get() = _name
-        set(value) {
-            if (value == _name) _name = value
-            else println("Вы не можете менять название корабля!")
-        }
-    val averageSpeed: Int
-        get() = _averageSpeed
-    val homePackage: String
-        get() = _homePackage
+class Ship(name: String, averageSpeed: Int, homePackage: String) {
+    var name: String = name
+        set(value) = println("Вы не можете менять название корабля!")
+    val averageSpeed: Int = averageSpeed
+    val homePackage: String = homePackage
 }
 
 fun main() {
