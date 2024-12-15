0121aa2304ea43bf5ae24cdd6497182416833c11
Alexander Kobelev <kobelef@mail.ru>
2024-12-15 19:46:37 +0400

    KS-18-1-Order

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -1,10 +1,10 @@
 package lesson_18
 
-class Order(numberOder: Int) {
+class Order(val number: Int) {
     fun printInfoOrder(items: Any) {
         when (items) {
-            is String -> println("Заказан товар: $items")
-            is List<*> -> println("Заказаны следующие товары: ${items.joinToString(", ")}")
+            is String -> println("Заказан товар №$number: $items")
+            is List<*> -> println("Заказаны следующие товары №$number: ${items.joinToString(", ")}")
             else -> println("Неправильный тип данных")
         }
     }
@@ -14,5 +14,5 @@ fun main() {
     val order1 = Order(1)
     val order2 = Order(2)
     order1.printInfoOrder("Гречка")
-    order2.printInfoOrder(listOf("яблоки", "вишня", "капуста"))
+    order2.printInfoOrder(listOf("Яблоки", "вишня", "капуста."))
 }
