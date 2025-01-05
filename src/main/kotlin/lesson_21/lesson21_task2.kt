bee90aa2ec7109907c797f7d6a542ebe882b24dc
Alexander Kobelev <kobelef@mail.ru>
2025-01-05 21:25:22 +0400

    KS-21-2-evenNumbersSum убрал лишний пробел

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -10,5 +10,4 @@ fun main() {
     val listNumbers: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val sumEvenNumbers = listNumbers.evenNumbersSum()
     println("Сумма четных чисел: $sumEvenNumbers")
-
 }
