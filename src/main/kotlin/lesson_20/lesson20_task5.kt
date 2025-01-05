83dd17f0793501e4d01baa9198cd524a67695909
Alexander Kobelev <kobelef@mail.ru>
2025-01-05 20:04:43 +0400

    KS-20-5-robot исправил

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -1,22 +1,26 @@
 package lesson_20
 
-fun main() {
-    val listPhrases: List<String> =
+class Robot {
+    private val listPhrases: List<String> =
         listOf("С добрым утром!", "Добрый день!", "Добрый вечер!", "Кушать подано!", "Спокойной ночи!")
-    var modifier: (String) -> String = { it }
+    var modifier1: (String) -> String = { it }
     fun say() {
         val phrase = listPhrases.random()
-        println(modifier(phrase))
+        println(modifier1(phrase))
     }
 
     fun setModifier(newModifier: (String) -> String) {
-        modifier = newModifier
+        modifier1 = newModifier
     }
-    say()
-    setModifier { phrase ->
+}
+
+fun main() {
+    val robot = Robot()
+    robot.say()
+    robot.setModifier { phrase ->
         phrase.split(" ").joinToString(" ") { it.reversed() }
     }
-    say()
+    robot.say()
 }
 
 
